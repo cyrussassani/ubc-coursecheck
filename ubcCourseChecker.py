8113215090669adffb8344c3c44ff3cc047b14b0
Cyrus Sassani <cs.lynn@ymail.com>
2014-11-27 14:40:13 -0800

    Compiled regular expression search pattern
    This is done to make the search more efficient if used several times
    within the script

--- ubcCourseChecker.py
+++ ubcCourseChecker.py
@@ -2,16 +2,15 @@ import urllib
 import urllib2
 import re
 
+totalSeats = re.compile("<td width=200px>Total Seats Remaining:</td>" + "<td align=left><strong>(.*?)</strong></td>")
+generalSeats = re.compile("<td width=200px>General Seats Remaining:</td>" + "<td align=left><strong>(.*?)</strong></td>")
+restrictedSeats = re.compile("<td width=200px>Restricted Seats Remaining\*:</td>" + "<td align=left><strong>(.*?)</strong></td>")
 
 def checkSeats(varCourse):
 
   url = varCourse
   ubcResp = urllib.urlopen(url);
   ubcPage = ubcResp.read();
-
-  totalSeats = "<td width=200px>Total Seats Remaining:</td>" + "<td align=left><strong>(.*?)</strong></td>"
-  generalSeats = "<td width=200px>General Seats Remaining:</td>" + "<td align=left><strong>(.*?)</strong></td>"
-  restrictedSeats = "<td width=200px>Restricted Seats Remaining\*:</td>" + "<td align=left><strong>(.*?)</strong></td>"
 
   t = re.search(totalSeats, ubcPage)
   g = re.search(generalSeats, ubcPage)
