6531784f7261e3802fe6bcafeab25686ef3e9bd2
Cyrus Sassani <cs.lynn@ymail.com>
2014-11-28 11:12:34 -0800

    Added notification
    Notifies by opening the URL of the course page. Decided against using
    any API’s or other forms of notification that would require extra
    registration/etc.

--- ubcCourseChecker.py
+++ ubcCourseChecker.py
@@ -1,36 +1,79 @@
 import urllib
 import urllib2
 import re
+import time
+import webbrowser
 
-totalSeats = re.compile("<td width=200px>Total Seats Remaining:</td>" + "<td align=left><strong>(.*?)</strong></td>")
-generalSeats = re.compile("<td width=200px>General Seats Remaining:</td>" + "<td align=left><strong>(.*?)</strong></td>")
-restrictedSeats = re.compile("<td width=200px>Restricted Seats Remaining\*:</td>" + "<td align=left><strong>(.*?)</strong></td>")
+# Notify that a course is available
+def notify():
+  print("Open browser")
+  webbrowser.open_new(course)
 
+# Delay to prevent sending too many requests
+def wait(varDelay):
+  time.sleep(varDelay) 
+
+# Scan webpage for seats
 def checkSeats(varCourse):
 
   url = varCourse
   ubcResp = urllib.urlopen(url);
   ubcPage = ubcResp.read();
 
+
+  # Search for the seat number element
   t = re.search(totalSeats, ubcPage)
   g = re.search(generalSeats, ubcPage)
   r = re.search(restrictedSeats, ubcPage)
 
+  # Find remaining seats
   if t:
-    print "Total Seats Remaining:", t.group(1)
+    if t.group(1) == '0':
+      return 0
   else:
     print "Can't locate seats"
 
   if g:
-    print "General Seats Remaining:", g.group(1)
+    if g.group(1) != '0':
+      return 1
   else:
     print "Can't locate seats"
     
   if r:
-    print "Restricted Seats Remaining:", r.group(1)
+    if r.group(1) != '0':
+      return 2
   else:
     print "Can't locate seats"
 
+# Search pattern (compiled for efficiency)
+totalSeats = re.compile("<td width=200px>Total Seats Remaining:</td>" + "<td align=left><strong>(.*?)</strong></td>")
+generalSeats = re.compile("<td width=200px>General Seats Remaining:</td>" + "<td align=left><strong>(.*?)</strong></td>")
+restrictedSeats = re.compile("<td width=200px>Restricted Seats Remaining\*:</td>" + "<td align=left><strong>(.*?)</strong></td>")
 
-course = raw_input("Enter course URL:")
-checkSeats(course)
+# Get course parameters
+course = raw_input("Enter course + section link:") #"file:///Users/cyrus/Box%20Sync/Programming/Python/UBC%20Course%20Fetch/apsc598x.html"
+acceptRestricted = raw_input("Allowed restricted seating? (y/n)")
+delay = int(raw_input("Check every _ seconds?"))
+
+# Prevent too low of a search rate/DOSing the website
+if delay < 5:
+  delay = 5
+
+# Confitional for determining whether to notify
+while True:
+  status = checkSeats(course)
+  print(status)
+  if status == 0:
+    print("No")
+    wait(delay)
+    continue
+  if status == 1:
+    notify()
+    break
+  if status == 2:
+    if acceptRestricted == "y":
+      notify()
+      break
+    elif acceptRestricted == "n":
+      wait(delay)
+      continue
